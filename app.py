47b3ef16b3aa99f2bc41885a49ceac82f1680122
Purnima82 <poornimas572@gmail.com>
2025-07-29 11:00:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+!pip install pandas scikit-learn fairlearn shap
+!pip install streamlit
+!pip install aif360
+import pandas as pd
+import sklearn
+import fairlearn
+import shap
+import streamlit
+import aif360
